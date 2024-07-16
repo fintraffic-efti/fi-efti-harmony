cc92431822e86178c5f7c301999eca3b954f740f
Matti Uusitalo <matti.uusitalo@solita.fi>
2024-07-16 15:05:36 +0300

    EFTI-359 configurable EFTI_HARMONY_PORT for startup script

--- files/bin/wait_for_harmony.sh
+++ files/bin/wait_for_harmony.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+if [ -z $EFTI_HARMONY_PORT ]
+then
+  EFTI_HARMONY_PORT=8443
+fi
+
 cmd="$@"
 
 sleepcount=0
-until curl --silent --output /dev/null --insecure https://127.0.0.1:8443; do
+until curl --silent --output /dev/null --insecure https://127.0.0.1:$EFTI_HARMONY_PORT; do
   # sleep max 120 iterations
   if (( "$sleepcount" < "120" )); then
     >&2 echo "Harmony is unavailable - sleeping"
