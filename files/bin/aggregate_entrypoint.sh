b13f644aed8ccaea726a80bb349af0a43f0459af
Tiandu Zhao <tiandu.zhao@solita.fi>
2025-10-30 10:29:37 +0200

    EFTI-957: v2.6.0 has built-it support for passing keystores and truststores as env variables

--- files/bin/aggregate_entrypoint.sh
+++ files/bin/aggregate_entrypoint.sh
@@ -3,7 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error when substituting.
 
-/opt/efti/bin/init_stores.sh "$EFTI_AP_KEYSTORE_BASE64" "$EFTI_AP_TRUSTSTORE_BASE64" "$EFTI_TLS_KEYSTORE_BASE64" "$EFTI_TLS_TRUSTSTORE_BASE64"
 /opt/efti/bin/init_wsplugin.sh $EFTI_WSPLUGIN_PROPERTIES_BASE64
 if [ -z "${EFTI_LOGBACK_XML_BASE64:-}" ]; then
   echo "EFTI_LOGBACK_XML_BASE64 not set, skipping logback initialization"
