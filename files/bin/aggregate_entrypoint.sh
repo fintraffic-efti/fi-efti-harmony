fbac74830c98c79f23555a324be7ae563acb99f3
Tiandu Zhao <tiandu.zhao@solita.fi>
2025-10-30 10:45:01 +0200

    EFTI-957: v2.6.0 has built-it support for configuring logback via env variable

--- files/bin/aggregate_entrypoint.sh
+++ files/bin/aggregate_entrypoint.sh
@@ -4,10 +4,5 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error when substituting.
 
 /opt/efti/bin/init_wsplugin.sh $EFTI_WSPLUGIN_PROPERTIES_BASE64
-if [ -z "${EFTI_LOGBACK_XML_BASE64:-}" ]; then
-  echo "EFTI_LOGBACK_XML_BASE64 not set, skipping logback initialization"
-else
-  /opt/efti/bin/init_logback.sh $EFTI_LOGBACK_XML_BASE64
-fi
 
 exec /usr/bin/harmony-entrypoint "$@"
